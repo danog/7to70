6c5f7af2f036bbc3f74d60d2b1fdf0705ca23fa8
Daniil Gentili <daniil@daniil.it>
2019-06-20 10:06:46 +0200

    Add return type remover

---
+++ src/NodeVisitors/VoidReturnTypeRemover.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace danog\Php7to70\NodeVisitors;
+
+use PhpParser\Node;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\Node\FunctionLike;
+
+class VoidReturnTypeRemover extends NodeVisitorAbstract
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function leaveNode(Node $node)
+    {
+        if (!$node instanceof FunctionLike) {
+            return;
+        }
+        if ($node->returnType !== "void") {
+            return;
+        }
+
+        $node->returnType = null;
+    }
+}
