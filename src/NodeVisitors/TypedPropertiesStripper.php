5d47d75487a90e8040f5fe6d1c7f7e9682411795
Daniil Gentili <daniil@daniil.it>
2020-02-26 13:02:10 +0100

    Strip type properties

---
+++ src/NodeVisitors/TypedPropertiesStripper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace danog\Php7to70\NodeVisitors;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Property as StmtProperty;
+use PhpParser\NodeVisitorAbstract;
+
+/**
+ * Removes type specifiers for properties (PHP 7.4).
+ */
+class TypedPropertiesStripper extends NodeVisitorAbstract
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function leaveNode(Node $node)
+    {
+        if (!($node instanceof StmtProperty)) {
+            //return;
+        }
+        $node->type = null;
+        \var_dump($node);
+    }
+}
